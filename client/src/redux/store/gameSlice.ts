13c0ddcd79a252b2fece272891e23096ab96ec53
Ivan Rubtsov <rubtsov.ivan97@gmail.com>
2023-06-06 13:42:28 +0300

    IHateUseEffect

--- client/src/redux/store/gameSlice.ts
+++ client/src/redux/store/gameSlice.ts
@@ -1,24 +1,29 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit"
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
-import { Category, GameType } from "../../types/gameTypes"
-import { RootState } from "./store"
+import { Category, GameType } from '../../types/gameTypes';
+import { RootState } from './store';
 
 const initialState: GameType = {
-    game: []
-}
+  game: [],
+  score: 0,
+};
 
 export const gameSlice = createSlice({
-    name: 'game',
-    initialState,
-    reducers: {
-      saveQuestion: (state, action: PayloadAction<Category> ) => {
-        state.game = action.payload
-      },
-    }
-})
+  name: 'game',
+  initialState,
+  reducers: {
+    saveQuestion: (state, action: PayloadAction<Category>) => {
+      state.game = action.payload;
+    },
+    saveScore: (state, action: PayloadAction<number>) => {
+      state.score = action.payload;
+    },
+  },
+});
 
-export const {saveQuestion} = gameSlice.actions
+export const { saveQuestion, saveScore } = gameSlice.actions;
 
-export const selectUser = (state: RootState) => state.game.game
+export const selectGame = (state: RootState) => state.game.game;
+export const selectScore = (state: RootState) => state.game.score;
 
-export default gameSlice.reducer
+export default gameSlice.reducer;
